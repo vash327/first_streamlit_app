88d5255369d0890d641df88a6726230b49cfc306
vash327 <42099323+vash327@users.noreply.github.com>
2022-12-16 02:00:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as slit
+
 slit.title('My Parents New Healthy Diner')
+
 ##added new lines
 slit.header('Breakfast Favorites')
 slit.text('🥣Omega 3 & Blueberry Oatmeal')
 slit.text('🥗Kale, Spinach & Rocket Smoothie')
 slit.text('🐔Hard-Boiled Free-Range Egg')
 slit.text('🥑🍞Aavacado Toast')
+
+##add new header
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
