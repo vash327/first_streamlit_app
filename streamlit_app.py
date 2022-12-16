f6e09036ee0f48a6d7eac32cc3c2ef329c37613b
vash327 <42099323+vash327@users.noreply.github.com>
2022-12-16 01:43:13 -0600

    Update streamlit_app.py
    added header and texts
    added an alias for streamlit and used the alias instead.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
-import streamlit
+import streamlit as slit
 streamlit.title('My Parents New Healthy Diner')
+##added new lines
+streamlit.header('Breaakfash Menu')
+slit.text('Bacon Egg and Cheese')
+slit.text('Omeaga 3 & Blueberry Oatmeal')
+slit.text('Hard boiled free range egg')
