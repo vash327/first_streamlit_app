8c91c23cc307230dd63c39b79f39ed35f973d70e
vash327 <42099323+vash327@users.noreply.github.com>
2022-12-28 18:33:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # slit.dataframe(my_fruit_list)
 
 slit.dataframe(fruits_to_show)
+
+#New Section to display fruitvice api response
+import requests
+fruitvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+slit.text(fruitvice_response)
