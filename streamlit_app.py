8b2ba8da3ff1e6140f99af83283c1d4260409c00
vash327 <42099323+vash327@users.noreply.github.com>
2022-12-28 23:27:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,7 @@ my_cur = my_cnx.cursor()
 ##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 slit.header("The fruit load list contains:")
-slit.dataframe(my_data_row)
+slit.dataframe(my_data_rows)
 
